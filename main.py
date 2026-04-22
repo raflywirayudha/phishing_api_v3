d4b593c0455ad5f54e96bf5a854bb98790c2db1f
raflywirayudha <112853148+raflywirayudha@users.noreply.github.com>
2026-04-22 19:34:03 +0700

    Refactor prediction logic to use tldextract for domain extraction and improve cache handling

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import time
 import asyncio
 from fastapi import FastAPI, Request
 from urllib.parse import urlparse
-
+import tldextract
 from core.lifespan import lifespan
 from schemas.prediction import URLRequest, PredictionResponse
 from services.blacklist_service import check_phishtank
@@ -12,94 +12,46 @@ from services.ml_service import ml_engine
 app = FastAPI(lifespan=lifespan)
 prediction_cache = {}
 
+extractor = tldextract.TLDExtract(include_psl_private_domains=True)
 
-#  # Berurutan
-# @app.post("/predict", response_model=PredictionResponse)
-# async def predict_phishing(request: URLRequest, fastapi_req: Request):
-#     url = request.url
-    
-#     # 1. CEK CACHE
-#     if url in prediction_cache:
-#         return prediction_cache[url]
+@app.get("/")
+async def root():
+    return {
+        "app": "Raphish API",
+        "status": "running",
+    }
 
-#     start_time = time.time()
-#     parsed_url = urlparse(url)
-#     hostname = (parsed_url.hostname or "").replace("www.", "")
-#     client = fastapi_req.app.state.requests_client
-
-#     try:
-#         # 2. EKSEKUSI SEKUENSIAL (Layer per layer dengan Short-Circuiting)
-        
-#         # Layer 1: Blacklist (Hanya lanjut jika tidak masuk blacklist)
-#         is_blacklisted = await check_phishtank(client, url)
-        
-#         if is_blacklisted:
-#             res = {
-#                 "status": "phishing", 
-#                 "layer": "blacklist", 
-#                 "probability": 1.0
-#             }
-#         else:
-#             # Layer 2: Whitelist (Hanya lanjut jika tidak masuk whitelist)
-#             is_whitelisted = await check_tranco(client, hostname)
-            
-#             if is_whitelisted:
-#                 res = {
-#                     "status": "safe", 
-#                     "layer": "whitelist", 
-#                     "probability": 0.0
-#                 }
-#             else:
-#                 # Layer 3: ML (Hanya dijalankan jika Layer 1 & 2 tidak memberikan hasil pasti)
-#                 ml_prob = await asyncio.to_thread(ml_engine.predict, url)
-#                 res = {
-#                     "status": "phishing" if ml_prob > 0.8 else "safe",
-#                     "layer": "ml",
-#                     "probability": round(ml_prob, 4)
-#                 }
-
-#         # Tambahkan latensi server
-#         res["total_server_latency_ms"] = round((time.time() - start_time) * 1000, 2)
-        
-#         # Simpan ke cache
-#         prediction_cache[url] = res
-#         return res
-
-#     except Exception as e:
-#         return {
-#             "status": "safe", 
-#             "layer": "error", 
-#             "probability": 0.0,
-#             "total_server_latency_ms": round((time.time() - start_time) * 1000, 2)
-#         }
-
- # Paralel
+# Paralel
 @app.post("/predict", response_model=PredictionResponse)
 async def predict_phishing(request: URLRequest, fastapi_req: Request):
     url = request.url
     
-    # 1. CEK CACHE
+    # Cek cache
     if url in prediction_cache:
         return prediction_cache[url]
 
     start_time = time.time()
-    parsed_url = urlparse(url)
-    hostname = (parsed_url.hostname or "").replace("www.", "")
+
     client = fastapi_req.app.state.requests_client
 
+    # Penggunaan tldextract
+    ext = extractor(url)
+    registered_domain = ext.top_domain_under_public_suffix
+    # top_domain_under_public_suffix otomatis menggabungkan domain + suffix (misal: uin-suska.ac.id)
+
     try:
-        # 2. EKSEKUSI PARALEL (Layer 1, 2, dan 3 dijalankan bersamaan)
+        # Eksekusi paralel (Layer 1, 2, dan 3 dijalankan bersamaan)
         # ml_engine.predict dijalankan di thread agar tidak memblokir async loop
         tasks = [
-            check_phishtank(client, url),             # Layer 1 (I/O Bound)
-            check_tranco(client, hostname),           # Layer 2 (I/O Bound)
-            asyncio.to_thread(ml_engine.predict, url) # Layer 3 (CPU Bound - Speculative)
+            check_phishtank(client, url),             
+            check_tranco(client, registered_domain),           
+            asyncio.to_thread(ml_engine.predict, url)
         ]
         
         # Menunggu semua hasil selesai dikumpulkan
-        is_blacklisted, is_whitelisted, ml_prob = await asyncio.gather(*tasks)
+        is_blacklisted, is_whitelisted_raw, ml_data = await asyncio.gather(*tasks)
 
-        # 3. LOGIKA PRIORITAS KEPUTUSAN
+        # Logika priotitas keputusan
         # Walaupun semua hasil sudah ada, kita tetap mengikuti aturan prioritas:
         if is_blacklisted:
             res = {
@@ -107,22 +59,32 @@ async def predict_phishing(request: URLRequest, fastapi_req: Request):
                 "layer": "blacklist", 
                 "probability": 1.0
             }
-        elif is_whitelisted:
+        elif is_whitelisted_raw and not ext.is_private:
             res = {
                 "status": "safe", 
                 "layer": "whitelist", 
                 "probability": 0.0
             }
         else:
+
+            # Jika is_private=True (seperti instaagrann.vercel.app), 
+            # sistem dipaksa masuk ke sini (Layer 3 - ML)
+            if ext.is_private:
+                print(f"🚩 {registered_domain} terdeteksi sebagai Private Suffix (PaaS). Wajib analisis ML.")
+
             res = {
-                "status": "phishing" if ml_prob > 0.8 else "safe",
+                "status": "phishing" if ml_data["probability"] > 0.4 else "safe",
                 "layer": "ml",
-                "probability": round(ml_prob, 4)
+                "probability": round(ml_data["probability"], 4),
+                "ml_features": ml_data["features"],
+                "domain": ext.top_domain_under_public_suffix
             }
 
-        # Tambahkan latensi server
+        # Latensi server
         res["total_server_latency_ms"] = round((time.time() - start_time) * 1000, 2)
-        
+        res["domain"] = registered_domain
+        res["subdomain"] = ext.subdomain if ext.subdomain else "-"
+
         # Simpan ke cache
         prediction_cache[url] = res
         return res
@@ -132,5 +94,7 @@ async def predict_phishing(request: URLRequest, fastapi_req: Request):
             "status": "safe", 
             "layer": "error", 
             "probability": 0.0,
-            "total_server_latency_ms": round((time.time() - start_time) * 1000, 2)
+            "subdomain": "error",
+            "domain": "error",
+            "total_server_latency_ms": round((time.time() - start_time) * 1000, 2),
         }
